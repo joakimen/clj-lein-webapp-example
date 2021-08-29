1300dc9427da4b993304e04740dd4646c8167eae
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-08-29 13:47:37 +0200

    Add ring and a basic request handler

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-lein-webapp-example "0.1.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring "1.9.4"]]
   :main ^:skip-aot clj-lein-webapp-example.core
   :profiles {:uberjar
              {:aot :all
--- src/clj_lein_webapp_example/core.clj
+++ src/clj_lein_webapp_example/core.clj
@@ -1,7 +1,12 @@
 (ns clj-lein-webapp-example.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.util.response :refer [response]])
   (:gen-class))
+
+(defn handler [request]
+  (response "Hello, World!"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-jetty handler {:port 3000}))
