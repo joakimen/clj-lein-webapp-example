554ec0abaac0aa6f345f0e5ae47ed23ff0a71d9b
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-08-29 11:41:30 +0200

    Add Lein boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# clj-lein-webapp-example
+
+Example-webapp in Clojure using [ring](https://github.com/ring-clojure/ring) and [reitit](https://github.com/metosin/reitit).
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clj-lein-webapp-example "0.1.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot clj-lein-webapp-example.core
+  :profiles {:uberjar {:aot :all}})
