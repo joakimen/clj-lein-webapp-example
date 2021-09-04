1529d0ab0a560210e7f55d2fa6cc4074950de506
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-09-05 00:36:22 +0200

    Add reitit (includes ring)

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject clj-lein-webapp-example "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.9.4"]]
-  :main ^:skip-aot clj-lein-webapp-example.core
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [metosin/reitit "0.5.15"]]
+  ;; :main ^:skip-aot clj-lein-webapp-example.core
+  :repl-options {:init-ns clj-lein-webapp-example.server}
   :profiles {:uberjar
              {:aot :all
               :uberjar-name "app.jar"}})
