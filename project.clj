89b3c58392cd14c7ef4bdfb42a83354dcc79ecbe
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-09-05 01:34:16 +0200

    Add "lein-ring"-plugin

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
                  [metosin/reitit "0.5.15"]]
   ;; :main ^:skip-aot clj-lein-webapp-example.core
   :repl-options {:init-ns clj-lein-webapp-example.server}
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler clj-lein-webapp-example.server/app}
   :profiles {:uberjar
              {:aot :all
               :uberjar-name "app.jar"}})
