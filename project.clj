c3efef79edeb46e226bb0a591791f9343f511019
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-09-05 02:18:51 +0200

    Bump project to after transition to ring/reitit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-lein-webapp-example "0.1.0"
+(defproject clj-lein-webapp-example "0.2.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [metosin/reitit "0.5.15"]]
